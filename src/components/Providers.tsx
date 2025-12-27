8702746298bfcbfc9b4d1f2ebd5886f9a9647099
jonocairns <jonocairns@gmail.com>
2025-12-27 20:25:34 +1300

    add on route change motion

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -4,6 +4,7 @@ import type { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 
 import { PullToRefresh } from "@/components/PullToRefresh";
+import { PageTransition } from "@/components/PageTransition";
 import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/Toaster";
 
@@ -23,7 +24,7 @@ export const Providers = ({
 				disableTransitionOnChange
 			>
 				<PullToRefresh />
-				{children}
+				<PageTransition>{children}</PageTransition>
 				<Toaster />
 			</ThemeProvider>
 		</SessionProvider>
